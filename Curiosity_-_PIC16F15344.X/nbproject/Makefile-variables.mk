f055cf1b846ef1eed4c268c8533392764d9d8d38
Karol Z <karol.zmyslowski@dgt.eu>
2018-09-27 16:15:09 +0200

    Inicjalny projekt dla PIC16F15344

---
+++ Curiosity_-_PIC16F15344.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# free configuration
+CND_ARTIFACT_DIR_free=dist/free/production
+CND_ARTIFACT_NAME_free=Curiosity_-_PIC16F15344.X.production.hex
+CND_ARTIFACT_PATH_free=dist/free/production/Curiosity_-_PIC16F15344.X.production.hex
+CND_PACKAGE_DIR_free=${CND_DISTDIR}/free/package
+CND_PACKAGE_NAME_free=curiosity-pic16f15344.x.tar
+CND_PACKAGE_PATH_free=${CND_DISTDIR}/free/package/curiosity-pic16f15344.x.tar
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=Curiosity_-_PIC16F15344.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/Curiosity_-_PIC16F15344.X.production.hex
+CND_PACKAGE_DIR_pro=${CND_DISTDIR}/pro/package
+CND_PACKAGE_NAME_pro=curiosity-pic16f15344.x.tar
+CND_PACKAGE_PATH_pro=${CND_DISTDIR}/pro/package/curiosity-pic16f15344.x.tar
