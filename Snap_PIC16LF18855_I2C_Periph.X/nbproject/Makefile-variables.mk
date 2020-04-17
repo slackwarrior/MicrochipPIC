b6c9a01a1ecabecd8495e8fa28a4111317033860
Karol Zmyslowski <karol.zmyslowski@gmail.com>
2020-04-17 22:46:07 +0200

    Initial commit for project - service of two IO Expanders on I2C bus

---
+++ Snap_PIC16LF18855_I2C_Periph.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Snap_PIC16LF18855_I2C_Periph.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Snap_PIC16LF18855_I2C_Periph.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=snappic16lf18855i2cperiph.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/snappic16lf18855i2cperiph.x.tar
